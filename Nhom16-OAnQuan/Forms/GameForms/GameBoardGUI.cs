e1e3081157a96989507c1dccd2bf3b8c674ed80d
Công Tùng Nguyễn <tungnguyen1562006@gmail.com>
2025-11-10 22:01:12 +0700

    Done phần Guide

---
+++ Nhom16-OAnQuan/Forms/GameForms/GameBoardGUI.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Nhom16_OAnQuan.Forms.GameForms
+{
+    public partial class GameBoardGUI : Form
+    {
+        public GameBoardGUI()
+        {
+            InitializeComponent();
+        }
+    }
+}
