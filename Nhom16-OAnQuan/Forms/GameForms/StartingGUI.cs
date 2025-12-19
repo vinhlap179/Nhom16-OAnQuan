f465f88fe5673650e0f05d5e76f3a9e22fda627a
chituong1506 <haru15062006@gmail.com>
2025-12-19 20:57:37 +0700

    kiểm tra lần cuối

--- Nhom16-OAnQuan/Forms/GameForms/StartingGUI.cs
+++ Nhom16-OAnQuan/Forms/GameForms/StartingGUI.cs
@@ -112,7 +112,7 @@ namespace Nhom16_OAnQuan.Forms.GameForms
                 dgvBXH.Columns.Add("Name", "Tên");
                 dgvBXH.Columns.Add("Wins", "Thắng");
 
-                dgvBXH.Columns[0].Width = 150;  // Cột Top nhỏ thôi
+                dgvBXH.Columns[0].Width = 140;  // Cột Top nhỏ thôi
                 dgvBXH.Columns[1].Width = 300; // Cột Tên
                 dgvBXH.Columns[2].Width = 150;  // Cột Thắng
 
